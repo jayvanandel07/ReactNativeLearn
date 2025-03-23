92481ab01b91284415e6e6aafac0442ffcf75a27
jayvanandel07 <j.vanandel07@gmail.com>
2025-03-23 16:16:14 +0530

    image and final touch

--- components/goalInput.js
+++ components/goalInput.js
@@ -1,5 +1,13 @@
+import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, TextInput, View, StyleSheet, Modal } from "react-native";
+import {
+  Button,
+  TextInput,
+  View,
+  StyleSheet,
+  Modal,
+  Image,
+} from "react-native";
 
 const GoalInput = (props) => {
   const [goalInputText, setGoalInputText] = useState("");
@@ -11,24 +19,36 @@ const GoalInput = (props) => {
     setGoalInputText("");
   };
   return (
-    <Modal visible={props.visible} animationType="slide">
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Your Course Goal!"
-          value={goalInputText}
-          onChangeText={goalInputHandler}
-        />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
-          </View>
-          <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancelHandler} />
+    //StatusBar Expo component
+    <>
+      <StatusBar style="dark" />
+      <Modal visible={props.visible} animationType="slide">
+        <View style={styles.inputContainer}>
+          <Image
+            style={styles.image}
+            source={require("../assets/images/goal.png")}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Your Course Goal!"
+            value={goalInputText}
+            onChangeText={goalInputHandler}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title="Cancel"
+                color="#f31282"
+                onPress={props.onCancelHandler}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button title="Add Goal" onPress={addGoalHandler} />
+            </View>
           </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+    </>
   );
 };
 
@@ -37,14 +57,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    borderBlockEndColor: "#cccc",
-    borderBottomWidth: 1,
-    padding: 20,
+    paddingInline: 20,
+    backgroundColor: "#6cdff6",
+  },
+  image: {
+    aspectRatio: 1 / 1,
+    width: 100,
+    height: 100,
+    margin: 10,
   },
   textInput: {
     width: "100%",
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#71717160",
+    backgroundColor: "#e8e8e8",
+    borderRadius: 5,
     padding: 8,
   },
   buttonContainer: {
