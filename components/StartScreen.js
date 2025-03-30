dfc440a51229047c9f9d7f67e2d5ed7afc834247
jayvanandel07 <j.vanandel07@gmail.com>
2025-03-30 18:45:30 +0530

    start screen input

--- components/StartScreen.js
+++ components/StartScreen.js
@@ -1,20 +1,52 @@
-import { Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import PrimaryButton from "./Buttons/PrimaryButton";
 
 const StartScreen = () => {
   return (
-    <View>
-      <Text>
-        <Text>Guess</Text> The Number
-      </Text>
+    <View style={styles.inputContainer}>
       <TextInput
-        placeholder="Enter the Number to guess!"
+        style={styles.numberInput}
+        placeholder="00"
+        maxLength={2}
         keyboardType="numeric"
+        placeholderTextColor={"#ddb42f7e"}
+        cursorColor={"#ddb52f"}
+        selectionHandleColor={"#ddb52f"}
       />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    margin: 20,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 10,
+    boxShadow: [
+      {
+        offsetX: 0,
+        offsetY: 0,
+        blurRadius: "20px",
+        spreadDistance: "3px",
+        color: "#00000053",
+        inset: false,
+      },
+    ],
+  },
+  numberInput: {
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    marginVertical: 8,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 
 export default StartScreen;
