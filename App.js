dd6c027f04de564a9b242f607ab6041ec33ab995
jayvanandel07 <j.vanandel07@gmail.com>
2025-03-31 13:21:42 +0530

    expo linear gradient

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { StyleSheet, Text, View } from "react-native";
 
 import StartScreen from "./components/StartScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <StartScreen />;
+  return (
+    <LinearGradient
+      style={styles.rootContainer}
+      colors={["#d0b869", "#d6b750", "#e0b118"]}
+    >
+      {/* Expo linear gradient : npx expo install expo-linear-gradient */}
+      <StartScreen />
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
