8f5a96cbb3943a198b422f66d61220ef5c3550a8
jayvanandel07 <j.vanandel07@gmail.com>
2025-03-31 13:29:02 +0530

    remove unwanted imports and change gradient colors

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 import StartScreen from "./components/StartScreen";
 import { LinearGradient } from "expo-linear-gradient";
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <LinearGradient
       style={styles.rootContainer}
-      colors={["#d0b869", "#d6b750", "#e0b118"]}
+      colors={["#d2bd78", "#d4b755", "#e0b118"]}
     >
       {/* Expo linear gradient : npx expo install expo-linear-gradient */}
       <StartScreen />
--- components/StartScreen.js
+++ components/StartScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "./Buttons/PrimaryButton";
 
 const StartScreen = () => {
